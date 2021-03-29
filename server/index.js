da2c03203d984bd0c66bd071aa4470486eb3ba29
danielazheleva <xdaniela.z@gmail.com>
2021-03-29 20:20:57 +0100

    parametrise port

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const app = require('./config/express');
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('API Server started and listening on port 3000');
 });
 
