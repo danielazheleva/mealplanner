e646379fbc2774bf0c9f4e1482554aa15c682c3c
danielazheleva <xdaniela.z@gmail.com>
2022-01-09 11:35:08 +0000

    hello world fast-api

---
+++ servernew/app/api/api.py
@@ -0,0 +1,7 @@
+from fastapi import APIRouter
+
+from app.api import endpoints
+
+
+api_router = APIRouter()
+api_router.include_router(endpoints.router, prefix="/recipes")
---
+++ servernew/app/api/endpoints.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+
+@router.get("/hello", status_code=200)
+def hello():
+    return {"message": "Hello World"}
