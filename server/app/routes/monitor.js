ff2b03f326b38a58f536db6c828157a4e341bbb1
danielazheleva <xdaniela.z@gmail.com>
2022-02-13 17:06:33 +0000

    delete old

--- server/app/routes/monitor.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const monitorCtrl = require('../controller/monitorController');
-
-const router = express.Router();
-
-router.route('/')
-    .get(monitorCtrl.createMetric);
-
-module.exports = router;
